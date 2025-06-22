bbdb1c2d7c59a8bc01e8146d2849f60ae730a08d
vivek-mishra12 <vivek77056@gmail.com>
2025-06-22 12:34:47 +0530

    Update Hero.jsx
    changed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
         <img src={profile_img} height={180} width={170}  alt="" />
         <h1><span>I'm Vivek Mishra,</span>from Electronics and Communication Engineering branch.</h1>
-        <p>I am a MERN Stack Developer. Alongside my development work, I’m continuously improving my understanding of Data Structures and Algorithms (DSA) using C++.</p>
+        <p>I’m a developer with growing experience in both MERN stack development and Data Structures & Algorithms (DSA) using C++. </p>
         <div className="hero-action">
             <div ><AnchorLink className="hero-connect" offset={50} href='#contact'>Connect With me </AnchorLink></div>
             <div className="hero-resume">My resume</div>
